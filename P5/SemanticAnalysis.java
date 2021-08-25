96eda13154f66b3b0dd70b2966a76f3a7994681b
Jenna Gallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-08-25 10:14:40 +0200

    P5

---
+++ P5/SemanticAnalysis.java
@@ -0,0 +1,397 @@
+
+import static java.lang.System.exit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author jenna
+ */
+public class SemanticAnalysis {
+
+    private List<TreeNode> tree;
+    private List<TableNode> table;
+    private List<TableNode> deleteList = new ArrayList<TableNode>();
+    private List<TreeNode> deleteTreeList = new ArrayList<TreeNode>();
+    private List<String> forLoopScopeList = new ArrayList<>();
+
+    public SemanticAnalysis() {
+    }
+
+    public SemanticAnalysis(List<TreeNode> tree) {
+        this.tree = tree;
+    }
+
+    public List<TreeNode> getTree() {
+        return tree;
+    }
+
+    public void setTree(List<TreeNode> tree) {
+        this.tree = tree;
+    }
+
+    public List<TableNode> getTable() {
+        return table;
+    }
+
+    public void setTable(List<TableNode> table) {
+        this.table = table;
+    }
+
+    public void buildTable() {
+        if (tree != null) {
+            TreeNode ptr = tree.get(1);
+            table = ptr.getTable();
+            if (ptr != null) {
+                table.add(new TableNode(ptr.textOfNode, ptr.getId(), ptr.terminal, ptr.scope, ptr.contents));
+                List<TreeNode> children = ptr.getChildren();
+                if (children != null) {
+                    ListIterator it = children.listIterator();
+                    while (it.hasNext()) {
+                        buildChildren((TreeNode) it.next());
+
+                    }
+
+                }
+            }
+        }
+
+    }
+
+    public void buildChildren(TreeNode t) {
+        if (t != null) {
+            table.add(new TableNode(t.textOfNode, t.getId(), t.terminal, t.scope, t.contents));
+            List<TreeNode> children = t.getChildren();
+            if (children != null) {
+                ListIterator it = children.listIterator();
+                while (it.hasNext()) {
+                    buildChildren((TreeNode) it.next());
+
+                }
+            }
+
+        }
+    }
+
+    public void checkProcAgainstVar(String name) {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.textOfNode.equals("variable") && t.contents.equals(name)) {
+                System.out.println("Semantic error variable and procedure have same name: " + name);
+                exit(0);
+            }
+        }
+    }
+
+    public void checkProcScope(String name, String scope) {
+        //System.out.println("SemanticAnalysis.checkProcScope()");
+        int count = 0;
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+
+            if (t.textOfNode.equals("UserDefinedNameDef") && t.contents.equals(name) && ((t.scope.contains(scope.substring(0, scope.length() - 2)) && scope.contains(t.scope.substring(0, t.scope.length() - 2))) || t.scope.contains(scope))) {
+                //System.out.println(t.contents+","+name+" "+t.scope+","+scope);
+                count++;
+                if (count == 2) {
+                    System.out.println("Semantic error two procedure declarations with same name: " + name + " under same scope:" + scope);
+                    exit(0);
+                }
+            }
+        }
+    }
+
+    public void checkProcedure() {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.textOfNode.equals("UserDefinedNameDef")) {
+                checkProcAgainstVar(t.contents);
+                checkProcScope(t.contents, t.scope);
+            } else if (t.textOfNode.equals("UserDefinedNameCall")) {
+                checkProcAgainstVar(t.contents);
+            }
+        }
+    }
+
+    public void checkVariableNames() {
+        int nameIndex = 0;
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.textOfNode.equals("variable") && t.vName == null) {
+                setVariableNames(t.contents, t.scope, "v" + nameIndex);
+                nameIndex++;
+            } else if (t.textOfNode.equals("variableFor")) {
+
+                setForVariableNames(t.contents, t.scope, "v" + nameIndex);
+                nameIndex++;
+            }
+        }
+        //System.out.println("Largest name index "+(nameIndex-1));
+    }
+
+    public void setVariableNames(String s, String scope, String name) {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.textOfNode.equals("variable") && t.contents.equals(s) && t.vName == null && (t.scope.contains(scope) || scope.contains(t.scope))) {
+                t.vName = name;
+            }
+        }
+    }
+
+    public void setForVariableNames(String s, String scope, String name) {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if ((t.textOfNode.equals("variableFor") || t.textOfNode.equals("variable")) && t.contents.equals(s) && t.scope.contains(scope)) {
+                t.vName = name;
+            }
+        }
+    }
+
+    public void renameTree() {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.textOfNode.equals("variable") || t.textOfNode.equals("variableFor")) {
+                TreeNode ptr = tree.get(1);
+
+                ptr.renameVariable(t.nodeID, t.vName);
+                
+            }
+            
+            if(t.delete){
+                  TreeNode ptr = tree.get(1);
+                ptr.deleteCode(t.nodeID);
+            }
+            
+            if(t.textOfNode.equals("UserDefinedNameDef") || t.textOfNode.equals("UserDefinedNameCall")){
+                  TreeNode ptr = tree.get(1);
+
+                ptr.renameDeF(t.nodeID, t.vName);
+            }
+
+        }
+
+    }
+
+    public boolean search(int id) {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.nodeID == id) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public void printTable() {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            System.out.println(t.nodeID + ") " + t.textOfNode + " " + t.contents + " " + t.scope + " " + t.vName);
+
+        }
+    }
+
+    public void checkSemantics() {
+        forLoopListBuild();
+         checkProcedure();
+        checkVariableNames();
+        matchDefsAndCalls();
+        
+        deleteDeadCode();
+        forLoopVarCheck();
+      
+        System.out.println("Semantic Analysis finished with no errors ");
+    }
+
+    public void printTree() {
+        tree.get(1).printChildrenScope(0);
+    }
+
+    public void matchDefsAndCalls() {
+        int nameIndex = 0;
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.textOfNode.equals("UserDefinedNameDef") && t.vName == null) {
+                boolean found = findMatchingProcCalls(t.contents, t.scope, "p" + nameIndex);
+                if (found == true) {
+                    t.vName = "p" + nameIndex;
+                    nameIndex++;
+                } else {
+                    // System.out.println("Semantic Error: procedure call with no matching definition "+t.contents);
+                    // exit(0);
+                }
+
+            }
+        }
+        ListIterator it2 = table.listIterator();
+        while (it2.hasNext()) {
+            TableNode t = (TableNode) it2.next();
+            if (t.textOfNode.equals("UserDefinedNameCall") && t.vName == null) {
+                System.out.println("Semantic error: Call without matching definition : " + t.contents);
+                 exit(0);
+            }
+        }
+
+        //System.out.println("Largest name index "+(nameIndex-1));
+    }
+
+    public boolean findMatchingProcCalls(String name, String scope, String newName) {
+        // System.out.println("name="+name+ " scope="+scope);
+        ListIterator it = table.listIterator();
+        boolean found = false;
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+
+            if (t.textOfNode.equals("UserDefinedNameCall") && t.contents.equals(name)) {
+                //System.out.println(" Def:"+t.contents + "Call:"+name);
+                if (t.scope.equals(scope)) {
+                    t.vName = newName;
+                    found = true;
+                } else if (t.scope.contains(scope) && (((t.scope.length() - 2) == scope.length()) || (scope.length() - 2) == t.scope.length())) {
+                    t.vName = newName;
+                    found = true;
+                } else if (scope.contains(t.scope) && (((t.scope.length() - 2) == scope.length()) || (scope.length() - 2) == t.scope.length())) {
+                    t.vName = newName;
+                    found = true;
+                } else if (forLoopScopeList.contains(t.scope)) {
+                    found= testNextScope(t, newName,scope,t.scope);
+                }
+            }
+        }
+        return found;
+    }
+    
+    public boolean testNextScope(TableNode t, String newName,String scope, String tScope){
+      
+        if(scope.startsWith(tScope.substring(0,tScope.length()-2))){
+            t.vName=newName;
+            return true;
+        }else if(forLoopScopeList.contains(tScope)){
+            return testNextScope(t, newName, scope, tScope.substring(0,tScope.length()-2));
+        }else{
+            return false;
+        }
+        
+    }
+
+    public void deleteDeadCode() {
+        ListIterator it = table.listIterator();
+
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if (t.textOfNode.equals("UserDefinedNameDef") && t.vName == null) {
+                String scope = t.scope;
+
+                deleteBranch(scope);
+                if (!deleteList.contains(t)) {
+                      t.delete=true;
+                    deleteList.add(t);
+                }
+                if (!deleteList.contains(table.get(t.nodeID - 3))) {
+                    (table.get(t.nodeID - 3)).delete=true;
+                    deleteList.add(table.get(t.nodeID - 3));
+                }
+
+            }
+        }
+          renameTree();
+
+        ListIterator it2 = deleteList.listIterator();
+        while (it2.hasNext()) {
+            TableNode t = (TableNode) it2.next();
+            //System.out.println(t.nodeID);
+            table.remove(t);
+        }
+
+    }
+
+    public void deleteBranch(String scope) {
+        ListIterator it2 = table.listIterator();
+        // System.out.println("Scope="+scope);
+        //  List<TableNode> delteList = new ArrayList<TableNode>();
+        while (it2.hasNext()) {
+            TableNode t2 = (TableNode) it2.next();
+
+            if (t2.scope.equals(scope) || t2.scope.startsWith(scope)) {
+                //System.out.println("ID="+t2.nodeID+"T="+t2.textOfNode);
+                // delteList.add(t2);
+                
+                if (!deleteList.contains(t2)) {
+                 
+                    t2.delete=true;
+                    deleteList.add(t2);
+                }
+            }
+        }
+
+    }
+
+    public void forLoopVarCheck() {
+        ListIterator it = table.listIterator();
+        while (it.hasNext()) {
+            TableNode t = (TableNode) it.next();
+            if ((t.textOfNode.equals("variableFor"))) {
+
+                int forID = t.nodeID;
+                String vName = t.vName;
+
+                ListIterator it2 = table.listIterator();
+                boolean v2 = false;
+                boolean v3 = false;
+                boolean v4 = false;
+                while (it2.hasNext()) {
+                    TableNode t2 = (TableNode) it2.next();
+                    //System.out.println(t2.vName);
+                    if (t2.vName != null && t2.vName.equals(vName) && t2.nodeID != forID) {
+                        if (t2.nodeID == (forID + 4)) {
+                            v2 = true;
+                        } else if (t2.nodeID == (forID + 9)) {
+                            v3 = true;
+                        } else if (t2.nodeID == (forID + 13)) {
+                            v4 = true;
+                        } else {
+                            if (it2.hasNext()) {
+                                TableNode t3 = (TableNode) it2.next();
+                                if (t3.textOfNode.equals("assignment")) {
+                                    System.out.println("Semantic error for loop counter being modified: " + t.vName + " under same scope:" + t.scope);
+                                    exit(0);
+                                }
+                                break;
+                            }
+
+                        }
+
+                    }
+                }
+                if (v2 && v3 && v4) {
+
+                } else {
+                    System.out.println("Semantic error not all loop counting variables the same: " + t.vName + " under same scope:" + t.scope);
+                    exit(0);
+                }
+            }
+        }
+
+    }
+
+    public void forLoopListBuild() {
+        ListIterator it2 = table.listIterator();
+        while (it2.hasNext()) {
+            TableNode t = (TableNode) it2.next();
+            if (t.textOfNode.equals("variableFor")) {
+                forLoopScopeList.add(t.scope);
+            }
+        }
+    }
+
+}
